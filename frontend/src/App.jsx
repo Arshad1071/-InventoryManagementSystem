f1cce36799eb445b90e3adf5f9b33f8055d26ebf
Arshad vp <Arshad1071@yahoo.in>
2026-07-16 15:08:29 +0530

    clean up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-// src/App.jsx
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// Layout
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
-// Pages
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -23,14 +20,12 @@ function Layout() {
 
       <div className="main">
         <Navbar />
-
         <div className="content">
           <Routes>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/users" element={<Users />} />
-
             <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Routes>
         </div>
@@ -43,11 +38,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Authentication */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* Dashboard Layout */}
         <Route path="/*" element={<Layout />} />
       </Routes>
     </BrowserRouter>
